c4efd7d58b13d2bcb444c2fdc388d07837311c00
Mikail Khan <mikail@mikail-khan.com>
2022-12-28 20:30:15 -0500

    reversed log

--- src/scripting.rs
+++ src/scripting.rs
@@ -52,7 +52,13 @@ impl Default for RhaiRes {
         let output = Arc::new(RwLock::new(String::new()));
 
         let logger = output.clone();
-        engine.on_print(move |s| logger.write().unwrap().push_str(&format!("{}\n", s)));
+        engine.on_print(move |s| {
+            let new_log = {
+                let previous = logger.read().unwrap();
+                format!("{}\n{}", s, previous)
+            };
+            *logger.write().unwrap() = new_log;
+        });
 
         engine
             .register_type::<KinematicBody>()
